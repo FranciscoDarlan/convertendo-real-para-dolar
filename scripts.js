284f611c0e7e66a505406ee0db1a2d4c04b40b8c
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2023-03-25 17:28:11 -0300

    troca-de-texto-value

--- scripts.js
+++ scripts.js
@@ -2,19 +2,36 @@
 
 const button = document.getElementById('button-convert') // aqui vou la html pegar meu botao pelo getElementById
 
-const dolar = 5.2 // preciso dar um valor 
+
+//const moedas = document.getElementById("moedas")
+//const optionDolar = document.getElementById("option-dolar")
+
+const dolar = 5.25 // preciso dar um valor 
+//const euro = 5.66 // valor euro
 
 const convertValues = () => { //criei essa funçao com aero funtions pela setinha, todo vez que for clicado vai aparecer na tela
-    
+
     const inputReais = document.getElementById("value-dinheiro").value  // preciso colocar .value no final, senao ele pega o placeholder do html
-// depois de pegar meu valor no input preciso fazer os calculos
+    // depois de pegar meu valor no input preciso fazer os calculos
 
+    const trocaTextReal = document.getElementById("text-real") // criando o caminho para mexer nesse elemento
+    const trcaTextDolar = document.getElementById("text-dolar") // criando o caminho para mexer nesse elemento
 
-    inputReais / dolar // se quiser enxergar preciso colocar dentro do console.log
+    trocaTextReal.innerHTML = inputReais //.innnerHTML troca o texto na tela 
+    trcaTextDolar.innerHTML = inputReais / dolar //.innnerHTML troca o texto na tela
 
+    inputReais / dolar // conta que o JS vai converter o dinheiro
 
-//      console.log(inputReais)   // consigo enxerga o valor da minha variavel (input)
- //   console.log('cliqui no botao') // consigo enxergar na tela o que esta acontecendo 
+    /*
+    if {moedas = optionDolar 
+        console.log(inputReais / dolar) // se quiser enxergar preciso colocar dentro do console.log
+    }else {
+     console.log(inputReais / euro)
+    }*/
+
+    //  console.log(inputReais)   // consigo enxerga o valor da minha variavel (input)
+    //   console.log('cliqui no botao') // consigo enxergar na tela o que esta acontecendo 
+
 }
 
 button.addEventListener('click', convertValues) // ouvidor de eventos , preciso criar uma funçao antes que faça o que eu quero
